9bd9c226da8f2b9601aea46b40e4f300a4ecf9ad
ksoliven <kate.c.soliven@gmail.com>
2019-10-08 16:32:19 -0400

    Updates

---
+++ precache-manifest.1430654c16f0761aa60a6dce41cc1711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66434db94a9ab19658b636bac481771d",
+    "url": "/todos-API/index.html"
+  },
+  {
+    "revision": "3acd7c85a99ba73144ed",
+    "url": "/todos-API/static/css/main.0fbefffc.chunk.css"
+  },
+  {
+    "revision": "15d60f14cea469c9b232",
+    "url": "/todos-API/static/js/2.c1b38d36.chunk.js"
+  },
+  {
+    "revision": "3acd7c85a99ba73144ed",
+    "url": "/todos-API/static/js/main.3ac5fb1e.chunk.js"
+  },
+  {
+    "revision": "5705f285b2d124354aca",
+    "url": "/todos-API/static/js/runtime-main.5705576f.js"
+  }
+]);
